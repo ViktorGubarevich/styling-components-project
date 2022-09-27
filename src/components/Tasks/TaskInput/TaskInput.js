66f9961b84517246924498bb5795700f86f1fadc
Viktor Gubarevich <viktor.gubarevich@zigzag.global>
2022-09-27 16:48:21 +0300

    Added styled-components and css-modules.

--- src/components/Tasks/TaskInput/TaskInput.js
+++ src/components/Tasks/TaskInput/TaskInput.js
@@ -1,36 +1,36 @@
 import { useState } from "react";
-// import styled from "styled-components";
 
 import Button from "../../UI/Button/Button";
-import styles from "./TaskInput.module.css";
+import styled from "styled-components";
+// import styles from "./TaskInput.module.css";
 
-// const FormControl = styled.div`
-//   margin: 0.5rem 0;
+const FormControl = styled.div`
+  margin: 0.5rem 0;
 
-//   & label {
-//     font-weight: bold;
-//     display: block;
-//     margin-bottom: 0.5rem;
-//     color: ${(props) => (props.invalid ? "red" : "black")};
-//   }
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+    color: ${(props) => (props.invalid ? "red" : "black")};
+  }
 
-//   & input {
-//     display: block;
-//     width: 100%;
-//     border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
-//     background: ${(props) =>
-//       props.invalid ? "rgb(243, 157, 157)" : "transparent"};
-//     font: inherit;
-//     line-height: 1.5rem;
-//     padding: 0 0.25rem;
-//   }
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
+    background: ${(props) =>
+      props.invalid ? "rgb(243, 157, 157)" : "transparent"};
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
 
-//   & input:focus {
-//     outline: none;
-//     background: #c8e1e4;
-//     border-color: #00358b;
-//   }
-// `;
+  & input:focus {
+    outline: none;
+    background: #c8e1e4;
+    border-color: #00358b;
+  }
+`;
 
 const TaskInput = ({ onAddTask }) => {
   const [inputText, setInputText] = useState("");
@@ -54,19 +54,18 @@ const TaskInput = ({ onAddTask }) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      {/* <FormControl className={!isInputValid && "invalid"}> */}
-      {/* <FormControl invalid={!isInputValid}>
-        <label>Задачи</label>
-        <input type="text" onChange={taskInputChangeHandler} />
-      </FormControl> */}
-      <div
+      {/* <div
         className={`${styles["form-control"]} ${
           !isInputValid && styles.invalid
-        } `}
+        }`}
       >
         <label>Задачи</label>
         <input type="text" onChange={taskInputChangeHandler} />
-      </div>
+      </div> */}
+      <FormControl invalid={!isInputValid}>
+        <label>Задачи</label>
+        <input type="text" onChange={taskInputChangeHandler} />
+      </FormControl>
       <Button type="submit">Добавить Задачу</Button>
     </form>
   );
